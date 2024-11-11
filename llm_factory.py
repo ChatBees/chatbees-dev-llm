8e0beaf89f4373898f9b5474ac84fed639360df9
Jun Luo <luo.junius@gmail.com>
2024-11-11 15:43:50 -0800

    support llama 3.1 8B instruct model

--- llm_factory.py
+++ llm_factory.py
@@ -5,6 +5,7 @@ from local_small_llm import LocalSmallLLM
 # https://huggingface.co/google/gemma-2-2b-it
 # https://huggingface.co/meta-llama/Llama-3.2-1B-Instruct
 # https://huggingface.co/meta-llama/Llama-3.2-3B-Instruct
+# https://huggingface.co/meta-llama/Llama-3.1-8B-Instruct
 LOCAL_COMPLETION_MODEL = os.environ.get(
     'ENV_LOCAL_COMPLETION_MODEL',
     default='google/gemma-2-2b-it',
@@ -15,7 +16,8 @@ def get_llm(model_id: str = LOCAL_COMPLETION_MODEL) -> LocalSmallLLM:
         case 'google/gemma-2-2b-it':
             from llm_local_gemma2 import LocalGemma2LLM
             return LocalGemma2LLM(model_id)
-        case 'meta-llama/Llama-3.2-1B-Instruct' | 'meta-llama/Llama-3.2-3B-Instruct':
+        case ('meta-llama/Llama-3.2-1B-Instruct' | 'meta-llama/Llama-3.2-3B-Instruct' |
+              'meta-llama/Llama-3.1-8B-Instruct'):
             from llm_llama import LlamaLlm
             return LlamaLlm(model_id)
         case _:
